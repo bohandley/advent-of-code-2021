a632688e32bee787e890c5a5acc63d49b0378d0d
Brendan O'Handley <bohandle@nd.edu>
2021-12-01 21:32:19 -0500

    solution 1

---
+++ dec-1/ReadFile.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {  
+  public static void main(String[] args) {  
+    int total = -1;
+    int prev = 0;
+    try {
+      File myObj = new File("nums.txt");
+      Scanner myReader = new Scanner(myObj);  
+      while (myReader.hasNextInt()) {
+        int cur = myReader.nextInt();
+
+        if(prev < cur)
+          total = total + 1;
+
+        prev = cur;
+      }
+
+      System.out.println(total);
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    } 
+  }  
+}
